7c3276bd70e2de2ec8ed556a69b2ecc15f28e5de
edwinArellano <edwinarellano77@gmail.com>
2026-04-23 18:51:50 -0400

    TABLAS CON ESTEROIDES

--- multiplicar.js
+++ multiplicar.js
@@ -1,10 +1,18 @@
 function generarTablas(){
+
+    let tabla=document.getElementById("txtTabla").value;
     let contenedor = document.getElementById("contenedor");
+    if (tabla == "" || isNaN(tabla)) {
+    contenedor.innerHTML = '<div class="fila">Por favor, escribe un número</div>';
+    return;
+}
+
+    
    
 
     let contenido = " ";
     for(let i=1; i<=10; i++){
-       contenido += '<div class="fila">5 x ' + i + ' = ' + (5*i) + '</div>';
+       contenido += '<div class="fila">'+ tabla + ' x ' + i + ' = ' + (tabla*i) + '</div>';
 
 
     }
