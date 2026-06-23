a9bbeddc57a3203ca09dcde373780b5e5c3707fb
Edwin Arellano <edwinarellano77@gmail.com>
2026-06-23 19:11:18 -0400

    INICIO EVALUACION 2

--- multiplicar.js
+++ multiplicar.js
@@ -1,22 +1,23 @@
-function generarTablas(){
+function generarTablas() {
 
-    let tabla=document.getElementById("txtTabla").value;
+    let tabla = document.getElementById("txtTabla").value;
     let contenedor = document.getElementById("contenedor");
     if (tabla == "" || isNaN(tabla)) {
-    contenedor.innerHTML = '<div class="fila">Por favor, escribe un número</div>';
-    return;
-}
+        contenedor.innerHTML = '<div class="fila">Por favor, escribe un número</div>';
+        return;
+    }
 
-    
-   
+
+
 
     let contenido = " ";
-    for(let i=1; i<=10; i++){
-       contenido += '<div class="fila">'+ tabla + ' x ' + i + ' = ' + (tabla*i) + '</div>';
+    for (let i = 1; i <= 10; i++) {
+        contenido += '<div class="fila">' + tabla + ' x ' + i + ' = ' + (tabla * i) + '</div>';
 
 
     }
 
-    contenedor.innerHTML= contenido;
+    contenedor.innerHTML = contenido;
+    console.log(contenido);
 
 }
