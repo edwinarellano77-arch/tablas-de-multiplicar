8a0d9366f9e87eacc1e65d626927fb1a8abce4af
bpgualotuna <brayan.gualotuna09@gmail.com>
2026-06-23 18:09:59 -0500

    prueba merge

--- multiplicar.js
+++ multiplicar.js
@@ -3,16 +3,16 @@ function generarTablas(){
     let tabla=document.getElementById("txtTabla").value;
     let contenedor = document.getElementById("contenedor");
     if (tabla == "" || isNaN(tabla)) {
-    contenedor.innerHTML = '<div class="fila">Por favor, escribe un número</div>';
+    contenedor.innerHTML = '<div class="fila">Por favor, escribe 2 numeros</div>';
     return;
 }
 
     
    
 
-    let contenido = " ";
+    let contenido = "Contenido Inicial";
     for(let i=1; i<=10; i++){
-       contenido += '<div class="fila">'+ tabla + ' x ' + i + ' = ' + (tabla*i) + '</div>';
+       contenido += '<div class="fila">'+ tabla + ' x 2' + i + ' =2 ' + (tabla*i) + '</div>';
 
 
     }
